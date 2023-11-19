04cb4851ae776879e4f26e93ddc17ca7a906f86f
John-507 <jambzz53@gmail.com>
2023-11-18 22:09:31 -0500

    ejemplo 2 - nuevos paths y query

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ app = FastAPI()
 @app.get('/')
 def read_root():
     return {'Hello': 'World%!'}
+
+#get con nuevas rutas(Paths) y consulta(Query)
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/calc")
+def calcular(operando_1:float, operando_2:float):
+    return{"suma":operando_1 + operando_2}
