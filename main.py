ac7f6461dad0f5bcaafc8bfb66ae946caaf693ea
SrJeisonA <aguilarjeison15@gmail.com>
2023-11-20 09:53:15 -0500

    Nueva estructura del programa y se importa el modelo  Item en el archivo main.py

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 from typing import Union
 from fastapi import FastAPI
-from pydantic import BaseModel 
+from models.item_model import item 
+
 
 #Creacion de aplicacion FastAPI
 app = FastAPI()
-
-
-class item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
 
 @app.get('/')
 def read_root():
